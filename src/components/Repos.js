37679f2e766e0039e705883139f9c8a3f84d855e
AAYUSH KUMAR <aayushrocksall@gmail.com>
2021-06-22 10:51:57 +0530

    Added Bar3D Component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -74,6 +74,7 @@ const Repos = () => {
         <Pie3D data={mostUsed} />
         <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
